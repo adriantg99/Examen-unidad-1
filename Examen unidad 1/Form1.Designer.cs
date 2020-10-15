04a2e73df81f481f47360eb35d90dcd913b8c9af
Adrian <Adrian@DESKTOP-FP5DQLC>
2020-10-14 17:09:50 -0700

    Agregar archivos de proyecto.

---
+++ Examen unidad 1/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Examen_unidad_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPrimero = new System.Windows.Forms.Button();
+            this.btnSegundo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnPrimero
+            // 
+            this.btnPrimero.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPrimero.Location = new System.Drawing.Point(184, 122);
+            this.btnPrimero.Name = "btnPrimero";
+            this.btnPrimero.Size = new System.Drawing.Size(179, 83);
+            this.btnPrimero.TabIndex = 0;
+            this.btnPrimero.Text = "Primer Boton";
+            this.btnPrimero.UseVisualStyleBackColor = true;
+            this.btnPrimero.Click += new System.EventHandler(this.btnPrimero_Click);
+            // 
+            // btnSegundo
+            // 
+            this.btnSegundo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSegundo.Location = new System.Drawing.Point(184, 245);
+            this.btnSegundo.Name = "btnSegundo";
+            this.btnSegundo.Size = new System.Drawing.Size(179, 83);
+            this.btnSegundo.TabIndex = 1;
+            this.btnSegundo.Text = "Segundo Boton";
+            this.btnSegundo.UseVisualStyleBackColor = true;
+            this.btnSegundo.Click += new System.EventHandler(this.btnSegundo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSegundo);
+            this.Controls.Add(this.btnPrimero);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPrimero;
+        private System.Windows.Forms.Button btnSegundo;
+    }
+}
+
---
+++ Examen unidad 1/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Examen_unidad_1
+{
+    public partial class Form1 : Form
+    {
+        int[] valor = new int[24];
+        int i = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPrimero_Click(object sender, EventArgs e)
+        {
+            
+                if (i < 25)
+                {
+                    valor[i] = i;
+                MessageBox.Show("El valor del numero actual es: " + valor[i]);
+                i += 1;
+
+            }
+            else
+            {
+                MessageBox.Show("Llego al valor maximo del contador");
+            }
+            
+
+        
+        }
+
+        private void btnSegundo_Click(object sender, EventArgs e)
+        {
+
+            if (i > 0)
+            {
+                i -= 1;
+
+                MessageBox.Show("Se ha restado un valor al vector, el valor actual es: "+i);
+            }
+            else
+            {
+                MessageBox.Show("No se le pueden restar más valores");
+            }
+            
+        }
+    }
+}
